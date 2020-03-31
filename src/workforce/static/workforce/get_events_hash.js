1bffbc1721a74900715120dccf6f133def693e1d
Gabriel Pedrosa <gabrielmip@protonmail.com>
2020-03-31 10:54:19 -0300

    Warning when there is new events in the agenda in the dumbest way possible. HREF in the image in a quick way to give the freedom to the user to zoom the image if wanted.

---
+++ src/workforce/static/workforce/get_events_hash.js
@@ -0,0 +1,29 @@
+function getEventsHash() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            processHashDifferenceCheck(this.responseText);
+        }
+    };
+    xhttp.open("GET", "/schedule_hash", true);
+    xhttp.send();
+}
+
+function processHashDifferenceCheck(newHash) {
+    const hashIsEqual = (newHash === events_hash);
+    document.getElementById('refresh-warner').style.display = hashIsEqual ? 'none' : 'flex';
+
+    if (hashIsEqual) {
+        setTimerToCheckHashDifference();
+    }
+}
+
+const getMinutesInMiliseconds = (minutes) => minutes * 60 * 1000;
+
+function setTimerToCheckHashDifference() {
+    setTimeout(() => {
+        getEventsHash();
+    }, getMinutesInMiliseconds(1));
+}
+
+getEventsHash();
--- src/workforce/views.py
+++ src/workforce/views.py
@@ -1,24 +1,49 @@
-import datetime
+from datetime import time, datetime, date
 
-from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
 
 
 def get_my_schedule(request):
     if not hasattr(request, 'user') or not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
 
-    today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
-    today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
-    today_events = request.user.worker.calendar.workevent_set.filter(start__range=(today_min, today_max))
     worker = request.user.worker
+    today_events = _get_today_events(worker)
+    events_hash = _calculate_event_hash(today_events)
 
     return render(request, 'my_schedule.html', {
         'worker_timezone': worker.timezone,
         'worker_name': worker.auth_user.first_name,
         'user': request.user,
+        'events_hash': events_hash,
         'today_events': today_events
     })
+
+
+def get_my_schedule_hash(request):
+    if not hasattr(request, 'user') or not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+
+    worker = request.user.worker
+    today_events = _get_today_events(worker)
+    events_hash = _calculate_event_hash(today_events)
+
+    return HttpResponse(events_hash)
+
+
+def _get_today_events(worker):
+    today_min = datetime.combine(datetime.today(), time.min)
+    today_max = datetime.combine(datetime.today(), time.max)
+
+    return worker.calendar.workevent_set.filter(start__range=(today_min, today_max))
+
+
+def _calculate_event_hash(work_events):
+    joined_work_events = '|'.join([
+        f"{event.user.email_address}|{event.start}"
+        for event in work_events
+    ])
+
+    return hash(joined_work_events)
