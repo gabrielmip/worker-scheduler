2e97773dbf2e2afba3ce9f366e7857a4168b1d52
Gabriel Pedrosa <gabrielmip@protonmail.com>
2020-03-31 12:07:16 -0300

    Added the transfer of production specific files in the fabfile

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -5,18 +5,20 @@ import fabric
 import sysrsync
 
 
-def send_source_files():
-    ssh_destination = 'personal-site'
-    destination = "/home/gabriel/worker-scheduler/src"
-    repo_base_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    source_files_root = os.path.join(repo_base_folder, "src")
+ssh_destination = 'personal-site'
+destination = "/home/gabriel/worker-scheduler"
+repo_base_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+source_files_root = os.path.join(repo_base_folder, "src")
 
+
+
+def send_source_files():
     create_destination_folder(destination, ssh_destination)
-    ignored_patterns = get_ignored_patterns(repo_base_folder, "src")
+    ignored_patterns = get_ignored_patterns("src")
 
     sysrsync.run(
         source=source_files_root,
-        destination=destination,
+        destination=os.path.join(destination, "src"),
         destination_ssh="personal-site",
         exclusions=ignored_patterns,
         verbose=True,
@@ -29,7 +31,7 @@ def create_destination_folder(destination_folder, ssh_host):
     connection.close()
 
 
-def get_ignored_patterns(repo_base_folder, work_tree):
+def get_ignored_patterns(work_tree):
     git_get_ignored_command = f'git -C {repo_base_folder} ls-files --exclude-standard -oi --directory'
     ignored_prefixes = (subprocess.check_output(
         git_get_ignored_command.split(),
@@ -44,5 +46,23 @@ def get_ignored_patterns(repo_base_folder, work_tree):
     ]
 
 
+def send_production_specific_files():
+    file_paths = [
+        'poetry.lock',
+        'src/project/production_settings_extension.py',
+        'pyproject.toml',
+        'src/project/emails.py',
+    ]
+
+    for file_path in file_paths:
+        sysrsync.run(
+            source=f"{repo_base_folder}/{file_path}",
+            destination=f"{destination}/{file_path}",
+            destination_ssh="personal-site",
+            verbose=True,
+            options=["-a"])
+
+
 if __name__ == "__main__":
     send_source_files()
+    send_production_specific_files()
