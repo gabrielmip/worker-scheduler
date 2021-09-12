df20fcbdf8dd048f7e82d8f797744dab95e7c535
Gabriel Pedrosa <gabrielmip@protonmail.com>
2021-09-12 16:55:59 -0300

    Fixed run_tests arg read

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-RUN_END_TO_END=${INCLUDE_E2E:-false}
+RUN_END_TO_END=${INCLUDE_E2E:-"false"}
 
-CHROME_BIN=/usr/bin/chromium find . -name '*.py' | INCLUDE_E2E=RUN_END_TO_END entr ./manage.py test "$@"
+CHROME_BIN=/usr/bin/chromium find . -name '*.py' | INCLUDE_E2E="$RUN_END_TO_END" entr ./manage.py test "$@"
