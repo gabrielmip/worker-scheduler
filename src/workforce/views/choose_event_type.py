9bcd992def445ca377d2d826fd6d2a394206e0a5
Gabriel Pedrosa <gabrielmip@protonmail.com>
2021-12-12 16:40:10 -0300

    Added other timeslots to event instructions so that the person can decide if they want to cancel

--- src/workforce/views/choose_event_type.py
+++ src/workforce/views/choose_event_type.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
 
 from workforce.services.events import get_user_next_event
-from workforce.services.free_timeslots import get_available_count
+from workforce.services.free_timeslots import get_available_count, get_free_timeslot_choices
+from workforce.utils import get_locale_from_settings
 from workforce.views.rules import finished_registration_required
 
 
@@ -9,10 +11,17 @@ from workforce.views.rules import finished_registration_required
 def choose_event_type(request, user):
     remote_count = get_available_count(is_live=False)
     live_count = get_available_count(is_live=True)
+    next_event = get_user_next_event(user.email_address)
+    other_timeslots = None if not next_event else get_free_timeslot_choices(
+        user.timezone,
+        locale=get_locale_from_settings(settings.LANGUAGE_CODE),
+        is_live=next_event.is_live,
+    )
 
     return render(request, 'choose_event_type.html', {
         'user': user,
-        'next_event': get_user_next_event(user.email_address),
+        'next_event': next_event,
+        'other_timeslots': other_timeslots,
         'has_timeslots_available': any([remote_count, live_count]),
         'count_remote_available': remote_count,
         'count_live_available': live_count,
